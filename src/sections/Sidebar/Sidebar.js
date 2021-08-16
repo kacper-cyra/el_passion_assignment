6e1464cfe46df807e6619e99073359c178d69153
kacper-cyra <kacper.cyranowski@gmail.com>
2021-08-16 18:48:00 +0200

    Sidebar

---
+++ src/sections/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { SidebarItem, SystemPanel } from "../../components";
+import { Closed, Github, Open } from "../../components/Icons";
+
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <SystemPanel />
+      <SidebarItem title="All" numberOfIssues="12">
+        <Github />
+      </SidebarItem>
+      <SidebarItem title="Open" numberOfIssues="4">
+        <Open />
+      </SidebarItem>
+      <SidebarItem title="Closed" numberOfIssues="8">
+        <Closed />
+      </SidebarItem>
+    </aside>
+  );
+}
